68254679bb9fbd490dc9f4672bd4d9bddf88b28b
Erik Silkensen <silkense@colorado.edu>
2012-10-14 16:06:57 -0400

    CPS CE-machine passes tests

--- cps-ce.rkt
+++ cps-ce.rkt
@@ -7,16 +7,17 @@
 (provide eval-n CPS+E cps-ce)
 
 ;; Semantics:
-(define (eval-n P)
-  (define results
-    (let ([S (term (,P ((k stop) (k (cl x (k x))))))])
-      (apply-reduction-relation* cps-ce S)))
-  (define match-reduction-result
-    (term-match/single CPS+E [((k x) ((x c) (k stop))) (term c)]))
-  (unless (= (length results) 1)
-    (error 'eval-d "term ~s had multiple reductions: ~s" (term P) results))
-  (match-reduction-result (car results)))
-
+(define-metafunction CPS
+  eval-n : W -> any
+  [(eval-n (λ (k) P))
+   ,(let* ([S (term (P ((k (cl x (k x) ((k stop)))))))]
+           [results (apply-reduction-relation* cps-ce S)])
+      (define match-reduction-result
+        (term-match/single CPS+E [((k x) ((k stop) (x c))) (term c)]))
+      (unless (= (length results) 1)
+        (error 'eval-d "term ~s had multiple reductions: ~s" (term P) results))
+      (match-reduction-result (car results)))])
+      
 ;; Data Specifications:
 (define-extended-language CPS+E CPS
   (S (P E))
@@ -37,8 +38,8 @@
         "ce2"]
    [--> ((if0 W P_1 P_2) E)
         ,(if (eq? (term (μ W E)) 0)
-             (term ((P_1 E)))
-             (term ((P_2 E))))
+             (term (P_1 E))
+             (term (P_2 E)))
         "ce3"]
    [--> ((W k W_1 ...) E)
         (P_1 (extend E_1 (k_1 x_1 ...) ((lookup E k) (μ W_1 E) ...)))
@@ -49,11 +50,11 @@
         (where (cl (k_1 x_1 ...) P_1 E_1) (μ W E))
         "ce5"]
    [--> ((O k W_1 ...) E)
-        (P_1 (extend E_1 (x) ((δ O (μ W_1 E) ...))))
+        (P_1 (extend E_1 (x) ((δ O ((μ W_1 E) ...)))))
         (where (cl x P_1 E_1) (lookup E k))
         "ce6"]
    [--> ((O (λ (x) P) W_1 ...) E)
-        (P (extend E (x) ((δ O (μ W_1 E) ...))))
+        (P (extend E (x) ((δ O ((μ W_1 E) ...)))))
         "ce7"]))
 
 ;; Converting values to machine values:
@@ -62,3 +63,14 @@
   [(μ c E) c]
   [(μ x E) (lookup E x)]
   [(μ (λ (k x ...) P) E) (cl (k x ...) P E)])
+
+;; -----------------------------------------------------------------------------
+
+(module+ test
+  (define p1 (term (+ (+ 2 2) (let (x 1) (+ x x)))))
+  (define p2 (term (if0 (let (x 1) (- x x)) 1 2)))
+  (define p3 (term (let (f (λ (x y) (* x y y))) (+ (f 2 3) (f 4 5)))))
+
+  (test-equal (term (eval-n (cps ,p1))) 6)
+  (test-equal (term (eval-n (cps ,p2))) 1)
+  (test-equal (term (eval-n (cps ,p3))) 118))
